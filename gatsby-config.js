8340727f776f02e3fb605444cf79e7f5bc024913
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2020-06-13 21:03:40 +0100

    add custom banner

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
       options: {
         name: `Cara - @lekoarts/gatsby-theme-cara`,
         short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        description: `Vlad Tutunea Website`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,

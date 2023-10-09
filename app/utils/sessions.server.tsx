e923e6b38b4646943c7c77fe246befcdff69841e
Vlad Tutunea <vlad.tutunea@protonmail.com>
2023-10-09 21:03:25 +0100

    base

---
+++ app/utils/sessions.server.tsx
@@ -0,0 +1,15 @@
+import { createCookieSessionStorage } from "@remix-run/node";
+import { createThemeSessionResolver } from "remix-themes";
+
+const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: "__remix-themes",
+    path: "/",
+    httpOnly: true,
+    sameSite: "lax",
+    secrets: ["s3cr3t"],
+    secure: process.env.NODE_ENV === "production",
+  },
+});
+
+export const themeSessionResolver = createThemeSessionResolver(sessionStorage);
